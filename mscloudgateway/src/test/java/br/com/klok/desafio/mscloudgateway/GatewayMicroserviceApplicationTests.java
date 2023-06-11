88c51992a0adc31110cdc5f83d69e8002803c454
Iriedson Souto <iriedson.souto@academico.ifpb.edu.br>
2023-06-11 17:14:40 -0300

    :sparkles:feat(mscloudgateway): Gateway api for microservice created

---
+++ mscloudgateway/src/test/java/br/com/klok/desafio/mscloudgateway/GatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.klok.desafio.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

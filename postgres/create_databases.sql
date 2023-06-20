f9558781b05085aca8e2a276e0bff3b169c29342
Iriedson Souto <iriedson.souto@academico.ifpb.edu.br>
2023-06-19 23:56:10 -0300

    :recycle:refactor: modified docker-compose to be able to communicate spring images

--- postgres/create_databases.sql
+++ postgres/create_databases.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE msclient-db;
-CREATE DATABASE mspayment-db;
-CREATE DATABASE mssale-db;
-CREATE DATABASE msproduct-db;
+CREATE DATABASE msclientdb;
+CREATE DATABASE mspaymentdb;
+CREATE DATABASE mssaledb;
+CREATE DATABASE msproductdb;
